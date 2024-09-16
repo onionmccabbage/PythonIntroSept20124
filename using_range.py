e0362dae7692f3d7eb0139f0a2142425b96d8903
onionmccabbage <onionmccabbage@live.com>
2024-09-16 12:29:03 +0100

    using range and boolean logic

---
+++ using_range.py
@@ -0,0 +1,18 @@
+# we can use 'range' to generate a series of integer values
+even_nums = range(0,101,2) # start, stop-before, step
+print(type(even_nums)) # a range object
+for i in even_nums:
+    print(i, end=', ') # we may choose to override the default end-of-line when printing
+
+# we can use a range to create a list
+odd_nums = list(range(1, 100, 2)) # make a list from a range - NB these values DO exist in memory
+print(odd_nums)
+
+# we can use these with boolean checks
+r = (4,5,6,88,2,9,33,42)
+for _ in r:
+    '''check if it is even or odd'''
+    if _ in even_nums:
+        print(f'{_} is even')
+    elif _ in odd_nums:
+        print(f'{_} is odd')

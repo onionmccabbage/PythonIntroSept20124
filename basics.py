7a1c6dcd013f4050591b69ce71246dc6dc03c24b
onionmccabbage <onionmccabbage@live.com>
2024-09-16 10:14:27 +0100

    basic data types and mathematical operators

---
+++ basics.py
@@ -0,0 +1,23 @@
+# this is a comment in Python
+# Every Python file is a 'module'
+# identifiers and basic data types
+a = 1 # here is an integer
+b = 7.3 # here we have a float
+# Python will infer the data type - Python is loosely typed
+print(a, type(a), b, type(b))
+# we may declare a variable to another value
+a = 3.5
+print(a, type(a))
+# maths
+print(a+b, a-b, a*b, a/b)
+print(b**a) # ** means raise to the power of
+# some other useful mathematical operators
+a = 2
+b = 7
+print(b//a, b%a) # // means modulo division (integer division). % is remainder
+
+# we also have...
+c = True # careful True, False, None have initial capital letter
+d = False
+e = None
+

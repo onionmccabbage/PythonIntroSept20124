c8574dbf9bf6e299ba4cef7501e6ba8b4e99d066
onionmccabbage <onionmccabbage@live.com>
2024-09-16 12:10:09 +0100

    using range object

--- user_input.py
+++ user_input.py
@@ -1,11 +1,27 @@
 def makeInt(x):
     '''If possible, convert x into an integer'''
-    i = int(x) # here we attempt type conversion
-    return i # when we reach a return statement, the function will end
+    # to be robust, first convert to a float then to an int
+    try:
+        i = int(float(x)) # here we attempt type conversion
+        return i # when we reach a return statement, the function will end
+    except ValueError as err:
+        return err # if there is a problem we return the string of the excetion
     print('this never happens')
 
 # we can ask the user to enter a value
-n = input('Please enter you name: ') # user input is ALWAYS a string
+n = input('Please enter your age: ') # user input is ALWAYS a string
 print(f'user entered: {n} which is {type(n)}')
 # use our function
-print( makeInt(n) )
+age = makeInt(n) 
+print( age )
+
+# we may check if values exist within a range
+# range(start, stop-before, step)
+if age in range(0,13): # the range object lets us easily handle a range of values
+    print(f'{age} is too young')
+elif age in range(13, 18):
+    print(f'{age} needs parental permission')
+elif age in range(18, 65):
+    print(f'{age} is acceptable here')
+else:
+    print(f'by {age} you should know better')

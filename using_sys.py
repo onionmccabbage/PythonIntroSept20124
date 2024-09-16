c7a47dfb78f05a4dd412858f8d19e01ca05d8c5f
onionmccabbage <onionmccabbage@live.com>
2024-09-16 16:04:34 +0100

    using sys

--- using_sys.py
+++ using_sys.py
@@ -5,9 +5,10 @@ if __name__ == '__main__':
     print(sys.version) # which version of Python
     print(sys.platform) # what platform are we on
     # we ALWAYS receive system argument variables
+    # Careful: sys.argv are ALWAYS string
     for s in sys.argv: # sys.argv is a tuple of system arguments
         # sys.argv[0] is ALWAYS the current module name and path
-        print(s)
+        print(f'Argument {s} is {type(s)}')
 
 # ways to terminate a running module
 # break # break out of a run-loop such as while

7658caa79869d8e81ed7b48b8668eea25666b2f0
onionmccabbage <onionmccabbage@live.com>
2024-09-16 16:00:20 +0100

    using sys

---
+++ using_sys.py
@@ -0,0 +1,14 @@
+import sys # sys is a really useful library - it gives system access
+
+if __name__ == '__main__':
+    ''' explore sys '''
+    print(sys.version) # which version of Python
+    print(sys.platform) # what platform are we on
+    # we ALWAYS receive system argument variables
+    for s in sys.argv: # sys.argv is a tuple of system arguments
+        # sys.argv[0] is ALWAYS the current module name and path
+        print(s)
+
+# ways to terminate a running module
+# break # break out of a run-loop such as while
+# sys.exit() # call the Python termination routine

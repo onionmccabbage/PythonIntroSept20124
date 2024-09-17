afabc265530d0dd8c9a50307a533f8fb259c0c05
onionmccabbage <onionmccabbage@live.com>
2024-09-17 12:13:51 +0100

    retrieving API endpoints using requests

---
+++ making_requests_to_API/call_api.py
@@ -0,0 +1,17 @@
+# make a call to an external API endpoint to retrieve structured data
+import requests # NB we need to pip install requests
+
+def fetchData():
+    '''make a request to a URL and return the retrieved data structure'''
+    url = 'https://jsonplaceholder.typicode.com/photos'
+    try:
+        response = requests.get(url) # we make a request to the url using 'get'
+        # this particular API endpoint returns JSON
+        data = response.json() # here we retrieve the returned JSON from the response
+        return data
+    except Exception as err:
+        print(err)
+
+if __name__ == '__main__':
+    result = fetchData()
+    print(result) # this is a very large amount of data!!!!

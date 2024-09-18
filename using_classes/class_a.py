a291c695b9a73453c36b3d3cf575f215318e1fad
onionmccabbage <onionmccabbage@live.com>
2024-09-18 10:43:23 +0100

    name mangling and __slots__

--- using_classes/class_a.py
+++ using_classes/class_a.py
@@ -5,6 +5,7 @@
 # By convention we use CamelCase but Python doesnt care
 class Weather():
     '''this class will encapsulate temperature and wind-speed'''
+    __slots__ = ['__temperature', '__windspeed']
     def __init__(self, t, w): # we must provide 'self' for any class methods
         '''this initialiser will be called every time we make an instance of this class'''
         self.temperature = t # even though this looks like a property, it calls a function
@@ -12,21 +13,25 @@ class Weather():
     # we often need to validate the properties of a class
     @property # this is how we declare a property GETTER (to retrieve a property from a class)
     def temperature(self):
-        return self.__temperature
+        return self.__temperature # leading double-underscore prevents direct access to the proeprty
     # temperature must be an in or a float
     @temperature.setter # thjis is how we declare a property SETTER (to mutate a property of a class)
     def temperature(self, new_t):
         if type(new_t) in (int, float):
+            # we use name-mangling to prevent direct access to properties
             self.__temperature = new_t
         else:
             self.__temperature = 12 # set a sensible default (we could raise exception)
     # temperature must be an in or a float
+    @property
+    def windspeed(self): # getter
+        return self.__windspeed
+    @windspeed.setter # setter
     def windspeed(self, new_w):
         if type(new_w) in (int, float):
-            self.windspeed = new_w
+            self.__windspeed = new_w
         else:
             raise(Exception) # raise exception
-
     # Every object implements __str__ so we override the built-in __str__ with our own
     def __str__(self):
         '''when we override __str__ we instruct on how to print'''
@@ -40,3 +45,9 @@ if __name__ == '__main__':
     # w1.windspeed = []
     # remember, print will ALWAYS invoke the __str__ of the object being printed
     print(w1)
+    # are we able to directly access __ properties (NO!!!)
+    # print( w1.__temperature )
+    # oh dear - we CAN addarbitrary properties to our instances
+    # ...unless we limit the __slots__
+    # w1.__temperature = 'not yet' # this adds an additional arbitrary property
+    # print(w1.__temperature)

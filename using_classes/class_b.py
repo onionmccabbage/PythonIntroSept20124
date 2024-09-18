7e580d32d9371e93d8c0ee8dd2629738178125be
onionmccabbage <onionmccabbage@live.com>
2024-09-18 11:32:15 +0100

    responding to property changes in classes

--- using_classes/class_b.py
+++ using_classes/class_b.py
@@ -1,12 +1,17 @@
 import class_a
+from random import randint
+import time
 
 class Equipment(class_a.Weather): # we may inherit from any other object
-    '''Equipment needs to monitor temp and wind, and control a/c '''
-    __slots__ = ['__temperaure', '__windspeed', '__name']
+    '''Equipment needs to monitor temp and wind, and control a/c 
+    if temp is less than 12 turn on heater, temp>28 turn on a/c'''
+    __slots__ = ['__temperature', '__windspeed', '__name', '__ac', '__heater']
     def __init__(self, t, w, name):
         # we call the init method of the parent class
         super().__init__(t,w)
         self.name = name
+        self.__heater = False
+        self.__ac = False
     @property
     def name(self):
         return self.__name # remember to use name-mangling
@@ -16,10 +21,30 @@ class Equipment(class_a.Weather): # we may inherit from any other object
             self.__name = new_name
         else:
             raise TypeError('Name must be a non empty string')
+    # we need  to monitor the temoperature and control heat/ a/c
+    def checkTemp(self):
+        '''return 'low' 'ok' or 'high' '''
+        if self.temperature <12: # call the getter property method of the parent class
+            return 'low'
+        elif self.temperature >28:
+            return 'high'
+        else:
+            return 'ok'
+    def controlClimate(self):
+        result = self.checkTemp()
+        if result=='low':
+            self.__heater = True
+            self.__ac     = False
+        elif result=='high':
+            self.__heater = False
+            self.__ac     = True
+        else:
+            self.__heater = False
+            self.__ac     = False
     # we usually override __str__
     def __str__(self):
         text = super().__str__() # call the __str__ of the parent class
-        text += f' name: {self.__name}'
+        text += f' name: {self.__name} Heater: {self.__heater} A/C: {self.__ac}'
         return text
 
 # you may choose to inherit from ANYTHING
@@ -32,3 +57,11 @@ class MyDict(dict):
 if __name__ == '__main__':
     mast = Equipment(13, 32, 'Dublin Mast')
     print(mast)
+    # play with the temperature
+    while True:
+        t = randint(-99, 100)
+        if t==100:
+            break
+        mast.temperature = t
+        mast.controlClimate()
+        print(mast)

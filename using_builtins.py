ebac65381d72af6cab8959cfa27868eb424ce046
onionmccabbage <onionmccabbage@live.com>
2024-09-18 15:22:52 +0100

    overriding built-in operators

--- using_builtins.py
+++ using_builtins.py
@@ -5,6 +5,10 @@ class Word:
         self.word = word # we could write validation property methods for this
     def __str__(self):
         return f'{self.word}'
+    # we may choose to override any of the built-in comparison operators
+    def __eq__(self, other_word):
+        # every str has .lower() for force lower case (also .upper())
+        return self.word.lower() == other_word.word.lower()
 
 if __name__ == '__main__':
     # normal comparison operators work as expected
@@ -12,6 +16,7 @@ if __name__ == '__main__':
     w2 = 'Hello'
     print( w1 == w2 ) # False
     # using our Word class
-    word1 = 'hello'
-    word2 = 'Hello'
+    word1 = Word('hello')
+    word2 = Word('Hello')
     print(word1, word2)
+    print(word1 == word2) # True - since we compare the lower-case version of each
